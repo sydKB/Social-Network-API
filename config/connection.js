5c8a1bf1ff182c9811d5ebbae2ed00057b5cbf34
sydKB <sydneykb11@icloud.com>
2023-08-22 22:07:26 -0700

    routes and starter seeds

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-connect('mongodb://localhost/thoughtsAndReactions', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = connection;
--- models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Reaction = require('./Reaction');
-const Thought = require('./Thought');
-
-module.exports = { User, Thought, Reaction };
